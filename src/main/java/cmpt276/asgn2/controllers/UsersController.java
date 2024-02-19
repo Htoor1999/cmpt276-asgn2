d25580a08e84bd3cf89ae9fcfb398077562e185f
Htoor1999 <103155951+Htoor1999@users.noreply.github.com>
2024-02-19 14:49:39 -0800

    models and controllers added

---
+++ src/main/java/cmpt276/asgn2/controllers/UsersController.java
@@ -0,0 +1,30 @@
+package cmpt276.asgn2.controllers;
+
+import java.util.List;
+
+import javax.management.modelmbean.ModelMBean;
+
+import java.util.ArrayList;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import cmpt276.asgn2.models.Users;
+
+@Controller
+public class UsersController {
+    @GetMapping("/users/view")
+   
+    public String getAllUsers(Model model){
+        System.out.println("Getting all users");
+       //get all users from database
+        List<Users> users = new ArrayList<>();
+        users.add(new Users("bobby","3001"));
+        users.add(new Users("john","3002"));
+        //end of database call
+
+        model.addAttribute("us", users);
+        return "users/showAll";
+    }
+}
---
+++ src/main/java/cmpt276/asgn2/models/Users.java
@@ -0,0 +1,30 @@
+package cmpt276.asgn2.models;
+
+public class Users {
+    private string name;
+    private int id;
+
+    public Users(){
+    }
+
+    public Users(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }//getter
+    public void setName(String name){
+        this.name = name;
+    }//setter
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+}
